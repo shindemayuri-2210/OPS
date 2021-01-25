20ca935e289f4ded681b638aedc7a4462e03ef58
shindemayuri-2210 <shindemayuri2210@gmail.com>
2021-01-25 13:28:09 +0530

    Service

--- online-passport-react-ui-app/src/index.js
+++ online-passport-react-ui-app/src/index.js
@@ -1,3 +1,28 @@
+//Mayuri
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
 /*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -25,7 +50,7 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();*/
+reportWebVitals();
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -57,3 +82,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
